6fd0d43fadcc999d5215aebf7629fcf2e7905863
Sandeep Malhotra <sandeep.virgo@hotmail.com>
2023-02-28 10:22:47 +1100

    Fix Issue for  Appollo Server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { createApplication  } from 'graphql-modules';
-import { bookModule } from './modules/index.js';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { createApplication } from "graphql-modules";
+import { bookModule } from "./modules/index.js";
+import { useGraphQLModules } from "@envelop/graphql-modules";
 
 const application = createApplication({
-    modules:[bookModule]
+  modules: [bookModule],
 });
-// Following commented lines of code is not working 
-// const executor = application.createApolloExecutor();
-// const typeDefs = application.typeDefs;
-// const resolvers = application.resolvers;
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//   });
+const server = new ApolloServer({
+  // schema: application.createSchemaForApollo() // graphql-modules has marked this deprecated, but works with Apollo Server 4.
+  // Recommended approach is to use gateway:
+  gateway: {
+    async load() {
+      return { executor: application.createApolloExecutor() };
+    },
+    onSchemaLoadOrUpdate(callback) {
+      callback({ apiSchema: application.schema } as any);
+      return () => {};
+    },
+    async stop() {},
+  },
+});
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-const schema = application.schema;
-const modules = application.ɵconfig.modules;
-const server = new ApolloServer({schema:schema});
-
-const { url } = await startStandaloneServer(server,{
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀  Server ready at: ${url}`);
